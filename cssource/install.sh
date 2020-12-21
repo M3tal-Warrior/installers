9e3177debca8e78369d41e6aa7a94b854709efb9
M3tal_Warrior <m3tal_warrior@private.localhost>
2020-12-21 19:57:33 +0100

    Updated version for Debian Bullseye; now rerunable for updates etc.

--- cssource/install.sh
+++ cssource/install.sh
@@ -8,7 +8,7 @@
 
 
 # ================================= Copyright =================================
-# Version 0.1.1 (2019-12-11), Copyright (C) 2019
+# Version 0.2.0 (2020-12-21), Copyright (C) 2019-2020
 # Author: Metal_Warrior
 # Coauthors: -
 
@@ -36,6 +36,34 @@ CI_USER_DEFAULT="cssource"
 
 
 # ================================= Functions =================================
+
+# Sets files as executable
+ci_setexec(){
+  # Takes the path to the file to operate on as parameter 1
+  # Returns 0 on success
+  # Returns 1 on failure
+  
+  # Local variables
+  local FILE="$1"
+  
+  # Check file
+  if [ "$FILE" = "" ]
+    then
+      return 1
+  elif [ ! -f "$FILE" ]
+    then
+      return 1
+  fi
+  
+  # Set file
+  if ! chmod a+x "$FILE" > /dev/null 2>&1
+    then
+      return 1
+  fi
+  
+  # Return success
+  return 0
+}
 
 ci_setvars() {
 # Default home of executing user
@@ -74,8 +102,22 @@ sed -i 's# main# main contrib non-free#g' /etc/apt/sources.list
 # Update and upgrade all packages
 apt-get update -qq && apt-get dselect-upgrade
 
+# Set dependencies for Debian versions, as packets sometimes are replaced
+# Import the os-release file
+if [ -f /etc/os-release ]
+  then
+    . /etc/os-release
+fi
+# Look if the system is a Debian Bullseye
+if echo "$PRETTY_NAME" | grep "bullseye" > /dev/null 2>&1
+  then
+    CI_DEPENDENCIES="lib32gcc-s1 libc6-i386"
+  else
+    CI_DEPENDENCIES="lib32gcc1 libc6-i386"
+fi
+
 # Install dependencies
-apt-get install lib32gcc1 libc6-i386 steamcmd
+apt-get install $CI_DEPENDENCIES steamcmd
 if [ "$?" != "0" ]
   then
     echo "These packages are critical - without them there's no install of CS:S!"
@@ -92,9 +134,26 @@ fi
 echo -e "You can change all defaults to your desire, but they're compliant with FHS and\nthus I don't advise to do so without pressing reasons.\nSimple pressing ENTER will use the defaults.\n"
 
 # Create user
-echo -e "\nChange the username of the server host (default: $CI_USER_DEFAULT)"
-read CI_USER
-CI_USER="${CI_USER:-$CI_USER_DEFAULT}"
+while true
+  do
+    echo -e "\nChange the username of the server host (default: $CI_USER_DEFAULT)"
+    read CI_USER
+    CI_USER="${CI_USER:-$CI_USER_DEFAULT}"
+    # Check if the user does not exist
+    if ! getent passwd "$CI_USER" > /dev/null 2>&1
+      then
+        break
+    fi
+    # User exists
+    echo -e "User $CI_USER already exists. Use it? (Y/n)"
+    read RESPONSE
+    # Continue, if the user is to be used
+    if [ "$RESPONSE" != "N" -a "$RESPONSE" != "n" ]
+      then
+        CI_SKIP_CREATE_USER="y"
+        break
+    fi  
+  done        
 # Reset all variables to reflect the users name
 ci_setvars
 echo -e "\nChange the users writable directory (default: $CI_HOME_DEFAULT)"
@@ -102,14 +161,18 @@ read CI_HOME
 CI_HOME="${CI_HOME:-$CI_HOME_DEFAULT}"
 # Create directory in case it contains nonexisting parents
 mkdir -p "$CI_HOME"
-if ! adduser --system "$CI_USER" --home "$CI_HOME" 2>/dev/null
+# Don't attempt to create the user if it's already existing
+if [ "$CI_SKIP_CREATE_USER" != "y" ]
   then
-    echo "User could not be created. Continue anyway? (y/N)"
-    read RESPONSE
-    if [ "$RESPONSE" != "y" -a "$RESPONSE" != "Y" ]
+    if ! adduser --system "$CI_USER" --home "$CI_HOME" 2>/dev/null
       then
-        echo "Abort by user."
-        exit 1
+        echo "User could not be created. Continue anyway? (y/N)"
+        read RESPONSE
+        if [ "$RESPONSE" != "y" -a "$RESPONSE" != "Y" ]
+          then
+            echo "Abort by user."
+            exit 1
+        fi
     fi
 fi
 
@@ -138,14 +201,27 @@ echo "Correcting wrong permissions..."
 
 # Correct all file modes
 find "$CI_HOME" -type f -exec chmod a-x {} \;
-chmod a+x "$CI_HOME/.steam/steamcmd/steamcmd.sh"
-chmod a+x "$CI_HOME/.steam/steamcmd/linux32/steamcmd"
-chmod a+x "$CI_HOME/.steam/steamcmd/linux32/steamerrorreporter"
-chmod a+x "$CI_HOME/.steam/steamcmd/siteserverui/linux64/siteserverui"
+# Nowadays some of these files seem to have moved, I'll leave the old paths in 
+# here nonetheless in case they return
+# Old paths
+ci_setexec "$CI_HOME/.steam/steamcmd/steamcmd.sh"
+ci_setexec "$CI_HOME/.steam/steamcmd/linux32/steamcmd"
+ci_setexec "$CI_HOME/.steam/steamcmd/linux32/steamerrorreporter"
+ci_setexec "$CI_HOME/.steam/steamcmd/siteserverui/linux64/siteserverui"
+# New paths
+ci_setexec "$CI_HOME/.local/share/Steam/steamcmd/steamcmd.sh"
+ci_setexec "$CI_HOME/.local/share/Steam/steamcmd/linux32/steamcmd"
+ci_setexec "$CI_HOME/.local/share/Steam/steamcmd/linux32/steamerrorreporter"
+ci_setexec "$CI_HOME/.local/share/Steam/steamcmd/siteserverui/linux64/siteserverui"
 find "$CI_INSTALLDIR" -type f -exec chmod a-x {} \;
-chmod a+x "$CI_INSTALLDIR/srcds_linux"
-chmod a+x "$CI_INSTALLDIR/srcds_run"
-chmod a+x "$CI_INSTALLDIR/bin/vpk_linux32"
+# These files actually are important for the server itself!
+ci_setexec "$CI_INSTALLDIR/srcds_linux" \
+  && ci_setexec "$CI_INSTALLDIR/srcds_run" \
+  && ci_setexec "$CI_INSTALLDIR/bin/vpk_linux32"
+if [ "$?" != "0" ]
+  then
+    echo "Some file modes in '$CI_INSTALLDIR' could not be set properly. \nRecheck that!"
+fi
 
 # Set logdir
 echo -e "\nChange the log directory for the service (default: $CI_LOGDIR_DEFAULT)"
@@ -242,76 +318,100 @@ clear
 echo -e "=== Server Setup ==="
 
 # Get the server config
-if wget -q https://raw.githubusercontent.com/M3tal-Warrior/installers/master/cssource/servercfg.template -O "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
+if [ -f "$CI_INSTALLDIR/cstrike/cfg/server.cfg" ]
   then
-    chmod 644 "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
-    # Ask for certain aspects of the server
-    echo -e "\nProvide the name your gameserver will display in CS:S"
-    read CI_SERVERNAME
-    # Login password
-    CI_LOGINPASSWD="$RANDOM$RANDOM$RANDOM$RANDOM"
-    while [ "$CI_LOGINPASSWD" != "$CI_RETRY" ]
-      do
-        echo -e "\nGameserver login password? (leave empty to allow everyone)"
-        read -s CI_LOGINPASSWD
-        echo -e "\nConfirm password"
-        read -s CI_RETRY
-      done
-    # Admin password
-    CI_ADMINPASSWD="$RANDOM$RANDOM$RANDOM$RANDOM"
-    while [ "$CI_ADMINPASSWD" != "$CI_RETRY" ]
-      do
-        echo -e "\n\nGameserver admin password? (do NOT leave empty!)"
-        read -s CI_ADMINPASSWD
-        echo -e "\nConfirm password"
-        read -s CI_RETRY
-        # Prevent an empty password here
-        if [ "$CI_RETRY" = "" ]
-          then
-            CI_RETRY="yes, you set that password!"
-        fi
-      done
-    # Rewrite the config template
-    sed -i "s#CI_SERVERNAME#$CI_SERVERNAME#" "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
-    sed -i "s#CI_LOGINPASSWD#$CI_LOGINPASSWD#" "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
-    sed -i "s#CI_ADMINPASSWD#$CI_ADMINPASSWD#" "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
-    # Ask if there's a wish to edit the file
-    echo -e "\nIt is advised to edit the config personally for further options.\nDo that now? (Y/n)"
+    echo -e "There already is a server config - replace it? (y/N)"
     read RESPONSE
-    if [ "$RESPONSE" != "n" -a "$RESPONSE" != "N" ]
+    if [ "$RESPONSE" = "y" -o "$RESPONSE" = "Y" ]
       then
-        vi "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
+        # Move the old config out of the way
+        CI_SAVENAME="server.cfg.$(date +%Y%m%d%H%M)"
+        mv "$CI_INSTALLDIR/cstrike/cfg/server.cfg" "$CI_INSTALLDIR/cstrike/cfg/$CI_SAVENAME"
+        echo "Backed up your old server.cfg as $CI_SAVENAME"
+        CI_DLCFG="y"
     fi
   else
-    echo "server.cfg template could not be downloaded, aborting..."
-    exit 1
-fi      
+    CI_DLCFG="y"
+fi
+# Download the new one
+if [ "$CI_DLCFG" = "y" ]
+  then
+    if wget -q https://raw.githubusercontent.com/M3tal-Warrior/installers/master/cssource/servercfg.template -O "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
+      then
+        chmod 644 "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
+        # Ask for certain aspects of the server
+        echo -e "\nProvide the name your gameserver will display in CS:S"
+        read CI_SERVERNAME
+        # Login password
+        CI_LOGINPASSWD="$RANDOM$RANDOM$RANDOM$RANDOM"
+        while [ "$CI_LOGINPASSWD" != "$CI_RETRY" ]
+          do
+            echo -e "\nGameserver login password? (leave empty to allow everyone)"
+            read -s CI_LOGINPASSWD
+            echo -e "\nConfirm password"
+            read -s CI_RETRY
+          done
+        # Admin password
+        CI_ADMINPASSWD="$RANDOM$RANDOM$RANDOM$RANDOM"
+        while [ "$CI_ADMINPASSWD" != "$CI_RETRY" ]
+          do
+            echo -e "\n\nGameserver admin password? (do NOT leave empty!)"
+            read -s CI_ADMINPASSWD
+            echo -e "\nConfirm password"
+            read -s CI_RETRY
+            # Prevent an empty password here
+            if [ "$CI_RETRY" = "" ]
+              then
+                CI_RETRY="yes, you set that password!"
+            fi
+          done
+        # Rewrite the config template
+        sed -i "s#CI_SERVERNAME#$CI_SERVERNAME#" "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
+        sed -i "s#CI_LOGINPASSWD#$CI_LOGINPASSWD#" "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
+        sed -i "s#CI_ADMINPASSWD#$CI_ADMINPASSWD#" "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
+      else
+        echo "server.cfg template could not be downloaded, aborting..."
+        exit 1
+    fi
+
+# Ask if there's a wish to edit the file
+echo -e "\nIt is advised to edit the config personally for further options.\nDo that now? (Y/n)"
+read RESPONSE
+if [ "$RESPONSE" != "n" -a "$RESPONSE" != "N" ]
+  then
+    vi "$CI_INSTALLDIR/cstrike/cfg/server.cfg"
+fi
 
 # Download the systemd unit file template
 echo -e "\nInstalling CS:S as a service..."
 # Avoid accidental overwriting of another unit file
-CI_UNITNAME="$CI_USER"
-while [ -f "/etc/systemd/system/$CI_UNITNAME.service" ]
+CI_UNITNAME="$CI_USER.service"
+while [ -f "/etc/systemd/system/$CI_UNITNAME" ]
   do
-    echo "$CI_UNITNAME.service is already existing. Overwrite? (y/N)"
+    echo "$CI_UNITNAME is already existing. Overwrite? (y/N)"
     read RESPONSE
     if [ "$RESPONSE" = "y" -o "$RESPONSE" = "Y" ]
       then
-        rm "/etc/systemd/system/$CI_UNITNAME.service"
+        rm "/etc/systemd/system/$CI_UNITNAME"
       else
         echo "Provide another name"
         read CI_UNITNAME
+        # Add .service to the unit name, if not provided
+        if ! echo "$CI_UNITNAME" | grep -E "\.service$" > /dev/null 2>&1
+          then
+            CI_UNITNAME="$CI_UNITNAME.service"
+        fi
     fi
   done
 # Download
-if wget -q https://raw.githubusercontent.com/M3tal-Warrior/installers/master/cssource/systemd_unit.template -O /etc/systemd/system/$CI_UNITNAME.service
+if wget -q https://raw.githubusercontent.com/M3tal-Warrior/installers/master/cssource/systemd_unit.template -O /etc/systemd/system/$CI_UNITNAME
   then
-    chmod 644 "/etc/systemd/system/$CI_UNITNAME.service"
+    chmod 644 "/etc/systemd/system/$CI_UNITNAME"
     # Modify the template
-    sed -i "s#CI_HOME#$CI_HOME#g" "/etc/systemd/system/$CI_UNITNAME.service"
-    sed -i "s#CI_INSTALLDIR#$CI_INSTALLDIR#g" "/etc/systemd/system/$CI_UNITNAME.service"
-    sed -i "s#CI_TMPDIR#$CI_TMPDIR#g" "/etc/systemd/system/$CI_UNITNAME.service"
-    sed -i "s#CI_USER#$CI_USER#g" "/etc/systemd/system/$CI_UNITNAME.service"
+    sed -i "s#CI_HOME#$CI_HOME#g" "/etc/systemd/system/$CI_UNITNAME"
+    sed -i "s#CI_INSTALLDIR#$CI_INSTALLDIR#g" "/etc/systemd/system/$CI_UNITNAME"
+    sed -i "s#CI_TMPDIR#$CI_TMPDIR#g" "/etc/systemd/system/$CI_UNITNAME"
+    sed -i "s#CI_USER#$CI_USER#g" "/etc/systemd/system/$CI_UNITNAME"
     # Enable the service
     systemctl daemon-reload
     systemctl enable "$CI_UNITNAME"
